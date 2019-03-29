1724f4bd82af36764b5e8b2eac6da4bec061dd2c
AlexandruIstrate <38143225+AlexandruIstrate@users.noreply.github.com>
2019-03-29 12:00:41 +0200

    Final revision.

--- ChronicyShareExtension/ShareViewController.swift
+++ ChronicyShareExtension/ShareViewController.swift
@@ -6,36 +6,87 @@
 //
 
 import Cocoa
+import Social
 
-class ShareViewController: NSViewController {
-
-    override var nibName: NSNib.Name? {
-        return NSNib.Name("ShareViewController")
-    }
+class ShareViewController: SLComposeServiceViewController {
 
     override func loadView() {
         super.loadView()
     
         // Insert code here to customize the view
-        let item = self.extensionContext!.inputItems[0] as! NSExtensionItem
-        if let attachments = item.attachments {
-            NSLog("Attachments = %@", attachments as NSArray)
-        } else {
-            NSLog("No Attachments")
-        }
+        self.title = NSLocalizedString("ChronicyShareExtension", comment: "Title of the Social Service")
+    
+        NSLog("Input Items = %@", self.extensionContext!.inputItems as NSArray)
     }
 
-    @IBAction func send(_ sender: AnyObject?) {
-        let outputItem = NSExtensionItem()
-        // Complete implementation by setting the appropriate value on the output item
+    override func didSelectPost() {
+        // Perform the post operation
+        // When the operation is complete (probably asynchronously), the service should notify the success or failure as well as the items that were actually shared
     
-        let outputItems = [outputItem]
+        let inputItem = self.extensionContext!.inputItems[0] as! NSExtensionItem
+    
+        let outputItem: NSExtensionItem = inputItem.copy() as! NSExtensionItem;
+        outputItem.attributedContentText = NSAttributedString(string: self.contentText);
+        // Complete implementation by setting the appropriate value on the output item
+        
+        for item: NSItemProvider in (outputItem.attachments ?? []) {
+            print("BEGIN");
+            getTextData(itemProvider: item);
+            print("END");
+        }
+    
+        let outputItems: [Any] = [outputItem];
+    
         self.extensionContext!.completeRequest(returningItems: outputItems, completionHandler: nil)
-}
+    }
 
-    @IBAction func cancel(_ sender: AnyObject?) {
+    override func didSelectCancel() {
+        // Cleanup
+    
+        // Notify the Service was cancelled
         let cancelError = NSError(domain: NSCocoaErrorDomain, code: NSUserCancelledError, userInfo: nil)
         self.extensionContext!.cancelRequest(withError: cancelError)
     }
 
+    override func isContentValid() -> Bool {
+        let messageLength = self.contentText.trimmingCharacters(in: CharacterSet.whitespaces).utf8.count
+        let charactersRemaining = 140 - messageLength
+        self.charactersRemaining = charactersRemaining as NSNumber
+        
+        if charactersRemaining >= 0 {
+            return true
+        }
+        
+        return false
+    }
+
+}
+
+extension ShareViewController {
+    private func getTextData(itemProvider: NSItemProvider) -> String? {
+        let typeIdentifier: String = kUTTypeText as! String;
+        
+        var result: String?;
+        
+        if itemProvider.hasItemConformingToTypeIdentifier(typeIdentifier) {
+            itemProvider.loadItem(forTypeIdentifier: typeIdentifier, options: nil) { (data: NSSecureCoding?, error: Error?) in
+                guard error == nil else {
+                    return;
+                }
+                
+                if let content = data {
+                    let newData: NSData = NSData(contentsOf: content as! URL)!;
+                    
+                    guard let d: Data = newData as Data else {
+                        return;
+                    }
+                    
+                    print(String(data: d, encoding: .utf8));
+                }
+//                let str: String = String(data: data!, encoding: .utf8);
+            }
+        }
+        
+        return result;
+    }
 }
