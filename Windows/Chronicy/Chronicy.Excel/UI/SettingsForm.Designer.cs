8be919fede6325c4058a94042b40650d4f8d1258
AlexandruIstrate <38143225+AlexandruIstrate@users.noreply.github.com>
2019-09-19 23:33:35 +0300

    Added connection settings to Excel

--- Windows/Chronicy/Chronicy.Excel/UI/SettingsForm.Designer.cs
+++ Windows/Chronicy/Chronicy.Excel/UI/SettingsForm.Designer.cs
@@ -48,6 +48,8 @@
             this.button4 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.tabControl1.SuspendLayout();
             this.generalTabPage.SuspendLayout();
             this.groupBox3.SuspendLayout();
@@ -56,6 +58,7 @@
             this.groupBox1.SuspendLayout();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -63,19 +66,22 @@
             this.tabControl1.Controls.Add(this.generalTabPage);
             this.tabControl1.Controls.Add(this.servicesTabPage);
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tabControl1.Location = new System.Drawing.Point(10, 10);
+            this.tabControl1.Location = new System.Drawing.Point(13, 12);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(780, 380);
+            this.tabControl1.Size = new System.Drawing.Size(1041, 468);
             this.tabControl1.TabIndex = 0;
             // 
             // generalTabPage
             // 
+            this.generalTabPage.Controls.Add(this.groupBox4);
             this.generalTabPage.Controls.Add(this.groupBox3);
-            this.generalTabPage.Location = new System.Drawing.Point(4, 22);
+            this.generalTabPage.Location = new System.Drawing.Point(4, 25);
+            this.generalTabPage.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.generalTabPage.Name = "generalTabPage";
-            this.generalTabPage.Padding = new System.Windows.Forms.Padding(3);
-            this.generalTabPage.Size = new System.Drawing.Size(772, 354);
+            this.generalTabPage.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.generalTabPage.Size = new System.Drawing.Size(1033, 439);
             this.generalTabPage.TabIndex = 0;
             this.generalTabPage.Text = "General";
             this.generalTabPage.UseVisualStyleBackColor = true;
@@ -83,9 +89,11 @@
             // groupBox3
             // 
             this.groupBox3.Controls.Add(this.showRibbonBrandingCheckBox);
-            this.groupBox3.Location = new System.Drawing.Point(6, 6);
+            this.groupBox3.Location = new System.Drawing.Point(8, 7);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(200, 100);
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox3.Size = new System.Drawing.Size(267, 123);
             this.groupBox3.TabIndex = 0;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Ribbon";
@@ -93,9 +101,10 @@
             // showRibbonBrandingCheckBox
             // 
             this.showRibbonBrandingCheckBox.AutoSize = true;
-            this.showRibbonBrandingCheckBox.Location = new System.Drawing.Point(7, 20);
+            this.showRibbonBrandingCheckBox.Location = new System.Drawing.Point(9, 25);
+            this.showRibbonBrandingCheckBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.showRibbonBrandingCheckBox.Name = "showRibbonBrandingCheckBox";
-            this.showRibbonBrandingCheckBox.Size = new System.Drawing.Size(135, 17);
+            this.showRibbonBrandingCheckBox.Size = new System.Drawing.Size(174, 21);
             this.showRibbonBrandingCheckBox.TabIndex = 0;
             this.showRibbonBrandingCheckBox.Text = "Show Ribbon Branding";
             this.showRibbonBrandingCheckBox.UseVisualStyleBackColor = true;
@@ -104,10 +113,11 @@
             // 
             this.servicesTabPage.Controls.Add(this.groupBox2);
             this.servicesTabPage.Controls.Add(this.groupBox1);
-            this.servicesTabPage.Location = new System.Drawing.Point(4, 22);
+            this.servicesTabPage.Location = new System.Drawing.Point(4, 25);
+            this.servicesTabPage.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.servicesTabPage.Name = "servicesTabPage";
-            this.servicesTabPage.Padding = new System.Windows.Forms.Padding(3);
-            this.servicesTabPage.Size = new System.Drawing.Size(772, 354);
+            this.servicesTabPage.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.servicesTabPage.Size = new System.Drawing.Size(1033, 439);
             this.servicesTabPage.TabIndex = 1;
             this.servicesTabPage.Text = "Services";
             this.servicesTabPage.UseVisualStyleBackColor = true;
@@ -119,10 +129,11 @@
             this.groupBox2.Controls.Add(this.label3);
             this.groupBox2.Controls.Add(this.usernameTextBox);
             this.groupBox2.Controls.Add(this.label2);
-            this.groupBox2.Location = new System.Drawing.Point(6, 184);
+            this.groupBox2.Location = new System.Drawing.Point(8, 226);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Padding = new System.Windows.Forms.Padding(10);
-            this.groupBox2.Size = new System.Drawing.Size(330, 164);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(13, 12, 13, 12);
+            this.groupBox2.Size = new System.Drawing.Size(440, 202);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Auto Connect";
@@ -130,18 +141,20 @@
             // passwordTextBox
             // 
             this.passwordTextBox.Enabled = false;
-            this.passwordTextBox.Location = new System.Drawing.Point(69, 87);
+            this.passwordTextBox.Location = new System.Drawing.Point(92, 107);
+            this.passwordTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.passwordTextBox.Name = "passwordTextBox";
-            this.passwordTextBox.Size = new System.Drawing.Size(248, 20);
+            this.passwordTextBox.Size = new System.Drawing.Size(329, 22);
             this.passwordTextBox.TabIndex = 6;
             this.passwordTextBox.UseSystemPasswordChar = true;
             // 
             // autoConnectCheckBox
             // 
             this.autoConnectCheckBox.AutoSize = true;
-            this.autoConnectCheckBox.Location = new System.Drawing.Point(13, 26);
+            this.autoConnectCheckBox.Location = new System.Drawing.Point(17, 32);
+            this.autoConnectCheckBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.autoConnectCheckBox.Name = "autoConnectCheckBox";
-            this.autoConnectCheckBox.Size = new System.Drawing.Size(65, 17);
+            this.autoConnectCheckBox.Size = new System.Drawing.Size(82, 21);
             this.autoConnectCheckBox.TabIndex = 2;
             this.autoConnectCheckBox.Text = "Enabled";
             this.autoConnectCheckBox.UseVisualStyleBackColor = true;
@@ -150,26 +163,29 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(8, 90);
+            this.label3.Location = new System.Drawing.Point(11, 111);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.Size = new System.Drawing.Size(69, 17);
             this.label3.TabIndex = 4;
             this.label3.Text = "Password";
             // 
             // usernameTextBox
             // 
             this.usernameTextBox.Enabled = false;
-            this.usernameTextBox.Location = new System.Drawing.Point(69, 54);
+            this.usernameTextBox.Location = new System.Drawing.Point(92, 66);
+            this.usernameTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.usernameTextBox.Name = "usernameTextBox";
-            this.usernameTextBox.Size = new System.Drawing.Size(248, 20);
+            this.usernameTextBox.Size = new System.Drawing.Size(329, 22);
             this.usernameTextBox.TabIndex = 5;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(8, 57);
+            this.label2.Location = new System.Drawing.Point(11, 70);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.Size = new System.Drawing.Size(73, 17);
             this.label2.TabIndex = 3;
             this.label2.Text = "Username";
             // 
@@ -178,19 +194,21 @@
             this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.serverAddressTextBox);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(6, 6);
+            this.groupBox1.Location = new System.Drawing.Point(8, 7);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(10);
-            this.groupBox1.Size = new System.Drawing.Size(330, 172);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(13, 12, 13, 12);
+            this.groupBox1.Size = new System.Drawing.Size(440, 212);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Web";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(216, 46);
+            this.button2.Location = new System.Drawing.Point(288, 57);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(101, 23);
+            this.button2.Size = new System.Drawing.Size(135, 28);
             this.button2.TabIndex = 2;
             this.button2.Text = "Restore Default";
             this.button2.UseVisualStyleBackColor = true;
@@ -198,17 +216,19 @@
             // 
             // serverAddressTextBox
             // 
-            this.serverAddressTextBox.Location = new System.Drawing.Point(88, 20);
+            this.serverAddressTextBox.Location = new System.Drawing.Point(117, 25);
+            this.serverAddressTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.serverAddressTextBox.Name = "serverAddressTextBox";
-            this.serverAddressTextBox.Size = new System.Drawing.Size(229, 20);
+            this.serverAddressTextBox.Size = new System.Drawing.Size(304, 22);
             this.serverAddressTextBox.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 23);
+            this.label1.Location = new System.Drawing.Point(4, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.Size = new System.Drawing.Size(106, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Server Address";
             // 
@@ -218,10 +238,11 @@
             this.panel1.Controls.Add(this.button4);
             this.panel1.Controls.Add(this.button3);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel1.Location = new System.Drawing.Point(0, 400);
+            this.panel1.Location = new System.Drawing.Point(0, 492);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Padding = new System.Windows.Forms.Padding(10);
-            this.panel1.Size = new System.Drawing.Size(800, 50);
+            this.panel1.Padding = new System.Windows.Forms.Padding(13, 12, 13, 12);
+            this.panel1.Size = new System.Drawing.Size(1067, 62);
             this.panel1.TabIndex = 1;
             // 
             // button1
@@ -230,9 +251,10 @@
             this.button1.BackColor = System.Drawing.Color.White;
             this.button1.FlatAppearance.BorderColor = System.Drawing.Color.DarkGray;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(555, 10);
+            this.button1.Location = new System.Drawing.Point(740, 12);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(74, 27);
+            this.button1.Size = new System.Drawing.Size(99, 33);
             this.button1.TabIndex = 9;
             this.button1.Text = "OK";
             this.button1.UseVisualStyleBackColor = false;
@@ -244,9 +266,10 @@
             this.button4.BackColor = System.Drawing.Color.White;
             this.button4.FlatAppearance.BorderColor = System.Drawing.Color.DarkGray;
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button4.Location = new System.Drawing.Point(715, 10);
+            this.button4.Location = new System.Drawing.Point(953, 12);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(74, 27);
+            this.button4.Size = new System.Drawing.Size(99, 33);
             this.button4.TabIndex = 8;
             this.button4.Text = "Apply";
             this.button4.UseVisualStyleBackColor = false;
@@ -258,9 +281,10 @@
             this.button3.BackColor = System.Drawing.Color.White;
             this.button3.FlatAppearance.BorderColor = System.Drawing.Color.DarkGray;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Location = new System.Drawing.Point(635, 10);
+            this.button3.Location = new System.Drawing.Point(847, 12);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(74, 27);
+            this.button3.Size = new System.Drawing.Size(99, 33);
             this.button3.TabIndex = 7;
             this.button3.Text = "Cancel";
             this.button3.UseVisualStyleBackColor = false;
@@ -271,19 +295,41 @@
             this.panel2.Controls.Add(this.tabControl1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel2.Name = "panel2";
-            this.panel2.Padding = new System.Windows.Forms.Padding(10);
-            this.panel2.Size = new System.Drawing.Size(800, 400);
+            this.panel2.Padding = new System.Windows.Forms.Padding(13, 12, 13, 12);
+            this.panel2.Size = new System.Drawing.Size(1067, 492);
             this.panel2.TabIndex = 2;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.checkBox1);
+            this.groupBox4.Location = new System.Drawing.Point(8, 137);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(267, 295);
+            this.groupBox4.TabIndex = 1;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Connection";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(7, 22);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(203, 21);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Connect to Service on Start";
+            this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // SettingsForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "SettingsForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -300,6 +346,8 @@
             this.groupBox1.PerformLayout();
             this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -326,5 +374,7 @@
         private System.Windows.Forms.CheckBox showRibbonBrandingCheckBox;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
