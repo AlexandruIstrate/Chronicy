d6bf8fbb2683acadf4a48676f27e85be6c52082e
AlexandruIstrate <38143225+AlexandruIstrate@users.noreply.github.com>
2019-10-14 23:52:52 +0300

    Updated the field templates

---
+++ Windows/Chronicy/Chronicy.Tests/Data/FieldTemplateTest.cs
@@ -0,0 +1,18 @@
+﻿using Chronicy.Data;
+using NUnit.Framework;
+
+namespace Chronicy.Tests.Data
+{
+    [TestFixture]
+    public class FieldTemplateTest
+    {
+        [Test]
+        public void TemplatesMatch()
+        {
+            FieldTemplate template1 = new FieldTemplate(new CustomField());
+            FieldTemplate template2 = new FieldTemplate(new CustomField());
+
+            Assert.True(template1.Matches(template2), "The two templates must match");
+        }
+    }
+}
