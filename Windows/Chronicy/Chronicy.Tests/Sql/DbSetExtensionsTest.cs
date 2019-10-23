317e6d4200700295d9c1adfac155dd8264320b0d
AlexandruIstrate <38143225+AlexandruIstrate@users.noreply.github.com>
2019-10-23 23:49:51 +0300

    Added missing database test

---
+++ Windows/Chronicy/Chronicy.Tests/Sql/DbSetExtensionsTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Chronicy.Tests.Sql
+{
+    [TestFixture]
+    public class DbSetExtensionsTest
+    {
+        [Test]
+        public void CanFind()
+        {
+
+        }
+
+        [Test]
+        public void CanFindAsync()
+        {
+
+        }
+    }
+}
