c0c60296de195cc1110024c94c06794968d1e470
AlexandruIstrate <38143225+AlexandruIstrate@users.noreply.github.com>
2019-12-02 23:55:19 +0200

    Updated CocoaPods dependencies

--- macOS/Pods/Target Support Files/Pods-ChronicyMacOS/Pods-ChronicyMacOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ChronicyMacOS : NSObject
-@end
-@implementation PodsDummy_Pods_ChronicyMacOS
-@end
--- macOS/Pods/Target Support Files/Pods-ChronicyShareExtension/Pods-ChronicyShareExtension-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ChronicyShareExtension : NSObject
-@end
-@implementation PodsDummy_Pods_ChronicyShareExtension
-@end
