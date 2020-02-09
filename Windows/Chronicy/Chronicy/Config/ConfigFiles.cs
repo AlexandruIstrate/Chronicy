843870d3ea10bd2a0ceb1de465959770fa07d2b0
AlexandruIstrate <38143225+AlexandruIstrate@users.noreply.github.com>
2020-02-09 23:59:13 +0200

    Added missing documentation

--- Windows/Chronicy/Chronicy/Config/ConfigFiles.cs
+++ Windows/Chronicy/Chronicy/Config/ConfigFiles.cs
@@ -7,9 +7,19 @@ namespace Chronicy.Config
     /// </summary>
     public static class ConfigFiles
     {
+        /// <summary>
+        /// The main application settings file.
+        /// </summary>
         public const string MainSettings = "appsettings.json";
+
+        /// <summary>
+        /// The database settings file.
+        /// </summary>
         public const string DatabaseSettings = "appsettings.Database.json";
 
+        /// <summary>
+        /// The development settings file.
+        /// </summary>
         public static string DevelopmentSettings = $"appsettings.{ Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production" }.json";
     }
 }
--- Windows/Chronicy/Chronicy/Config/Settings.cs
+++ Windows/Chronicy/Chronicy/Config/Settings.cs
@@ -2,11 +2,29 @@
 {
     public static class Settings
     {
+        /// <summary>
+        /// Provides constants for setting names.
+        /// </summary>
         public static class Database
         {
+            /// <summary>
+            /// The data source setting.
+            /// </summary>
             public const string DataSource = "Settings:Database:DataSource";
+
+            /// <summary>
+            /// The database name setting.
+            /// </summary>
             public const string InitialCatalog = "Settings:Database:InitialCatalog";
+
+            /// <summary>
+            /// The username setting.
+            /// </summary>
             public const string UserID = "Settings:Database:UserID";
+
+            /// <summary>
+            /// The password setting.
+            /// </summary>
             public const string Password = "Settings:Database:UserID";
         }
     }
